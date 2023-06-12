bc703b23804c01c63f9b25e11495f9682583952f
Oshadhi Pabasara Kumari Ekanayaka <98898738+Oshadhi2github@users.noreply.github.com>
2023-06-12 15:17:43 +0530

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+
+import './App.css';
+
+//components
+import NavBar from './components/NavBar';
+import ContactBook from './components/ContactBook';
+import AllUsers from './components/AllUsers';
+import AddUser from './components/AddUser';
+import EditUser from './components/EditUser';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <NavBar/>
+      <Routes>
+      <Route path='/' element={<ContactBook/>}/>
+      <Route path='/all' element={<AllUsers/>}/>
+      <Route path='/add' element={<AddUser/>}/>
+      <Route path='/edit/:id' element={<EditUser/>}/>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ client/src/components/ContactBook.jsx
@@ -0,0 +1,10 @@
+
+
+
+const ContactBook = () => {
+    return(
+        <p>Hello from Contact Book</p>
+    )
+}
+
+export default ContactBook;
